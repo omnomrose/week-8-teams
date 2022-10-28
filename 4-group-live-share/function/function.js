2068a80a1ea1a94d6bf9182fa951c32c24886d40
carolinalxu <loocarolina07@gmail.com>
2022-10-28 15:26:18 -0700

    added files

---
+++ 4-group-live-share/function/function.js
@@ -0,0 +1,20 @@
+// Reorganize and fix this code so it is properly formatted
+// One if statement checks for Integer data type
+// One if statement checks for String data type
+// One else statement that is the default
+// The correct if/else statement will be stored in the sentence varaible
+// console log out the sentence at the end and add a single line comment.
+
+var z = 2;
+var sentence = "";
+
+if(z==2) {
+    console.log("The number is the string 2");
+} else if (z=="2") {
+    console.log("The number is something else");
+} else { 
+    console.log("The number is something else");
+}
+console.log(sentence);
+
+//The number is the string 2
---
+++ 4-group-live-share/object/object.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ * 17-2 Create a student object that has the first name value of Mark, last name value as Jacob, age as 31, and the id as A001234.
+ * Then initialise the variable sentence to say the following: 
+ * The new employee __first name__   __last name__ is __age__ years old. The employee ID is __empyloyee ID__.
+ * Then console log the sentence.
+ *
+ */
